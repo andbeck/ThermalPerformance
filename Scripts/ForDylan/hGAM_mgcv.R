e843759a4363a45e9946b722664e02a9f6757fb6
andbeck <andrew.beckerman@gmail.com>
2019-01-02 09:02:11 +0000

    updated gams with full Clone Treatment Experiment Factor to match peerJ

--- Scripts/ForDylan/hGAM_mgcv.R
+++ Scripts/ForDylan/hGAM_mgcv.R
@@ -17,9 +17,14 @@ glimpse(Fec_scale)
 Fec_scale <- Fec_scale %>% ungroup %>% 
   mutate(Clone = factor(Clone, ordered = FALSE)) %>% 
   mutate(Temperature = as.numeric(Temperature)) %>% 
+  # create factor that represents experimental treatments
   mutate(TrExp = factor(paste(Treatment,Experiment, sep = "_"), 
+                        ordered = FALSE)) %>% 
+  # create factor that represents clone and experimental treatments
+  # use unite?
+  mutate(ClTrExp = factor(paste(Clone, Treatment,Experiment, sep = "-"), 
                         ordered = FALSE))
-  
+
 # Quick View of the data again.
 ggplot(Fec_scale, aes(x = Temperature, y = Fec,
                       group = Clone, colour = Clone))+
@@ -27,13 +32,31 @@ ggplot(Fec_scale, aes(x = Temperature, y = Fec,
   geom_smooth(method = lm, formula = y ~ poly(x,2), se = FALSE)+
   facet_wrap(~TrExp, ncol = 2)
 
+#plot by 'collapsed factor'
+ggplot(Fec_scale, aes(x = Temperature, y = Fec, group = ClTrExp))+
+  geom_point()+
+  geom_smooth(method = lm, formula = y ~ poly(x,2), se = F)
+
 ## MGCV gam models with Fecundity ----
 
-## These seem to be the models to use... complex model is favoured
-# This works - I think this might be the good model to use
-# Temp x TrExp as baseline
-# clone as re
-# Group level trends, similar smoothers
+## These are one version of the models to use... 
+## treat clone as random, TrExp as factor
+
+# The More Complex Model (model 5)
+# group level trends with different smoothers
+# gives different baseline (mean) to each treatment
+# and alows differences among clones within treatments
+# does not reveal much among clone variation in wiggle
+# might be what we want
+# not sure what to make m =  and k = 
+mod2_5 <- gam(Fec ~ s(Temperature, by = TrExp, bs = "tp", k = 5)+
+                s(TrExp, by = Clone, bs = "re")+
+                s(Temperature, by = Clone, bs = "re"),
+              data = Fec_scale,
+              drop.unused.levels = FALSE, 
+              method="REML")
+
+# Model4: group level trends, similar smoothers
 mod2_4 <- gam(Fec ~ s(Temperature, by = TrExp, bs = "tp", k = 4)+
                 s(Clone, bs = "re"),
               data = Fec_scale,
@@ -41,16 +64,6 @@ mod2_4 <- gam(Fec ~ s(Temperature, by = TrExp, bs = "tp", k = 4)+
               method="REML")
 
 plot(mod2_4, pages = 1)
-
-# The More Complex Model
-# gives different baseline (mean) to each treatment
-# and alows differences among clones within treatments
-mod2_5 <- gam(Fec ~ s(Temperature, by = TrExp, bs = "tp", k = 4)+
-                s(TrExp, by = Clone, bs = "re")+
-                s(Temperature, by = Clone, bs = "re"),
-              data = Fec_scale,
-              drop.unused.levels = FALSE, 
-              method="REML")
 
 ## complex model justified
 anova(mod2_5, mod2_4, test = "Chisq")
@@ -67,6 +80,7 @@ plot(mod2_5, select = 4, scale = 0, ylab = "Predator - Acute",
      shade = TRUE)
 
 ## Plotting random effects detail
+# does not reveal very high levels of clonal variation
 newX <- expand.grid(Temperature = seq(from = 13, to = 28, by = 0.5),
                     Clone = levels(Fec_scale$Clone),
                     TrExp = levels(Fec_scale$TrExp))
@@ -95,64 +109,35 @@ p4 <- ggplot(plotThese, aes(x = Temperature, y = fittedFec,
 
 gridExtra::grid.arrange(p4, p5, ncol = 2)
 
+## Model with Clone, Treatment, Experiment grouping variable
+# Model 3 specification here: group level trends different smoothers
+# but single baseline
+# higher variation revealed, but not much
+# does not use 'experimental design'
+# m = 1 creates strong penalty and limited smooth
+# setting k = 3 creates smoothest picture... 
+# higher k generate limited smoothness and Odd shapes
+mod_ClTrExp <- gam(Fec ~ s(Temperature, bs = "tp", k = 3, m = 2)+
+                     s(Temperature, by = ClTrExp, bs = "ts", k = 3, m = 1)+
+                     s(ClTrExp, bs = "re", k = 40),
+                     data = Fec_scale,
+                     drop.unused.levels = FALSE, 
+                     method="REML")
 
-# # This is the simplest model, but lacks the Treatments and Experiment
-# 
-# mod2_1 <- gam(Fec ~ s(Temperature, bs = "tp", k = 4)+
-#               s(Temperature, by = Clone, bs = "re"),
-#             data = Fec_scale,
-#             drop.unused.levels = FALSE, 
-#             method="REML")
-# 
-# plot(mod2_1, pages = 1)
-# 
-# 
-# # This works but....
-# # tp spline for base
-# # Treatment_Experiment as re
-# # Clone nested in Treatment_Experiment as re
-# mod2_2 <- gam(Fec ~ s(Temperature, bs = "tp", k = 4)+
-#                 s(TrExp, bs = "re")+
-#                 s(TrExp, Clone, bs = "re"),
-#               data = Fec_scale,
-#               drop.unused.levels = FALSE, 
-#               method="REML")
-# 
-# plot(mod2_2, pages = 1)
-# 
-# # newXs
-# newX <- expand.grid(Temperature = seq(13, 28, 0.1),
-#                     TrExp = unique(Fec_scale$TrExp),
-#                     Clone = unique(Fec_scale$Clone))
-# 
-# # predictions
-# pred_mod2_2 <- predict(mod2_2, se.fit=TRUE, newdata = newX)
-# 
-# # housekeeping
-# plotThese <- transform(newX, fits = pred_mod2_2$fit, fit_se = pred_mod2_2$se.fit)
-# 
-# # plot
-# ggplot(data=plotThese, aes(x=Temperature, y=fits, group=Clone, colour = Clone)) +
-#   facet_wrap(~TrExp) +
-#   geom_line() +
-#   geom_point(data = Fec_scale, aes(x = Temperature, y = Fec, 
-#                                    group = Clone, colour = Clone))+
-#   geom_ribbon(aes(ymin=fits-2*fit_se,
-#                   ymax=fits+2*fit_se, fill = Clone), alpha=0.1, colour = NA)
-# 
-# 
-# # this doesn't work
-# # tp for base
-# # Temperature by TrExp as factor spline
-# # Temperature by clone as re
-# mod2_3 <- gam(Fec ~ s(Temperature, bs = "tp", k = 4)+
-#               s(Temperature, by = TrExp, bs = "fs")+
-#               s(Temperature, by = Clone, bs = "re"),
-#             data = Fec_scale,
-#             drop.unused.levels = FALSE, 
-#             method="REML")
-# 
-# 
-# 
-# 
-#   
+
+plot(mod_ClTrExp)
+
+## Plotting random effects detail
+newX <- expand.grid(Temperature = seq(from = 13, to = 28, by = 0.5),
+                    ClTrExp = unique(Fec_scale$ClTrExp))
+
+newY <- predict(mod_ClTrExp, newdata = newX, type = "response")
+plotThese <- data.frame(newX, fittedFec = newY) %>% 
+  separate(col = ClTrExp, into = c("Clone","Treatment","Experiment"), sep = "-")
+
+#need to get core smooth onto plots
+ggplot(plotThese, aes(x = Temperature, y = fittedFec,
+                            group = Clone, colour = Clone))+
+  geom_line()+
+  ylim(0,50)+
+  facet_grid(Experiment ~ Treatment)
